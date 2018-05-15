5260a63233f30aa7ee7e11a280588885101fa77f
WestEUspy <33495579+WestEUspy@users.noreply.github.com>
2018-05-15 19:12:53 +0100

    Update cat.js
    Updated cat.js so it would always reply that it doesn't work instead of just bugging everytime.

--- commands/cat.js
+++ commands/cat.js
@@ -2,6 +2,8 @@ const superagent = require("superagent");
 
 module.exports.run = async (bot, message, args) => {
 
+	return message.channel.send('"L.cat is under maintenance, sorry."');
+
   let {body} = await superagent
   .get(`http://aws.random.cat/meow`);
 
